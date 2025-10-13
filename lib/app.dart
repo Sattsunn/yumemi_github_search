26adef5a7fff7985eb315b9752474ed5277c65a9
Sattsunn <satsusarii@gmail.com>
2025-10-13 12:33:46 +0900

    feat: add theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yumeimi_github_search/core/providers/theme_provider.dart';
+import 'core/theme.dart';
 import 'home.dart';
 
-class GitHubSearchApp extends StatelessWidget {
+class GitHubSearchApp extends ConsumerWidget {
   const GitHubSearchApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeModeProvider);
+
     return MaterialApp(
       title: 'GitHub Search',
-      themeMode: ThemeMode.system,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: themeMode,
       home: const Home(),
     );
   }
--- lib/core/providers/theme_provider.dart
+++ lib/core/providers/theme_provider.dart
@@ -1,1 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
+// アプリ全体のテーマモード管理
+final themeModeProvider = StateProvider<ThemeMode>((ref) => ThemeMode.system);
