39119efe3c5ab716a65152accc3011f080a191c2
Sattsunn <satsusarii@gmail.com>
2025-10-13 12:30:19 +0900

    chore: add riverpod dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yumeimi_github_search/app.dart';
 
 void main() {
-  runApp(const GitHubSearchApp());
+  runApp(const ProviderScope(child: GitHubSearchApp()));
 }
